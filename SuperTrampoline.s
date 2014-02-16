9937bcc7bbd0868a4e09fb62c7f922b979599eb1
Kyle Sluder <kyle@ksluder.com>
2014-02-15 22:58:11 -0800

    Ensure alignment of _trampolineImp.

--- SuperTrampoline.s
+++ SuperTrampoline.s
@@ -4,6 +4,7 @@
 //   %rdi: pointer to ObjCSuper instance
 .text
 .globl __trampolineImp
+.align 4, 0x90
 __trampolineImp:
     movq _OBJC_IVAR_$_ObjCSuper._super(%rip), %r11
     leaq (%rdi, %r11), %rdi
